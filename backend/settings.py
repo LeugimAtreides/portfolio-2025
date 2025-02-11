efeccb15901cd82cb769d2eff47aea0938a81241
Miguel Villarreal <mvillarreal789@hotmail.com>
2025-02-11 16:06:49 -0500

    Add WhiteNoise middleware and update requirements for static file handling

--- backend/settings.py
+++ backend/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "corsheaders.middleware.CorsMiddleware",  # Must be before CommonMiddleware
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
