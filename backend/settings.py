1c5e4d0ce21426965c5c8a8895337a7bca32213f
Miguel Villarreal <mvillarreal789@hotmail.com>
2025-02-12 16:22:05 -0500

    Add S3 storage configuration and update CKEditor file storage settings

--- backend/settings.py
+++ backend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_ckeditor_5",
     "corsheaders",  # For CORS support
+    "storages",  # For S3 storage
 ]
 
 MIDDLEWARE = [
@@ -141,7 +142,7 @@ STATICFILES_DIRS = [
 CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=Csv())
 
 # CKEditor Configurations
-CKEDITOR_5_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+CKEDITOR_5_FILE_STORAGE = config("CKEDITOR_5_FILE_STORAGE")
 CKEDITOR_5_FILE_UPLOAD_PERMISSION = "staff"
 CKEDITOR_5_CUSTOM_CSS = "/static/custom.css"
 
@@ -317,6 +318,14 @@ if not DEBUG:
     SECURE_HSTS_SECONDS = 31536000  # 1 Year
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+
+    AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME", default="us-east-1")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
 # Logging configuration
 LOGGING = {
